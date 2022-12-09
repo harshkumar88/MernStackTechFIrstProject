1c36c990c7cd579f25f41e34764cfccc374bb191
harshkumar <harsh1559.be20@gmail.com>
2022-12-09 22:41:20 +0530

    adding server

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const app=express();
 const port=process.env.PORT || 5000;
 const registerRouter=require('./src/router/register.js')
 const path=require("path");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 require("./src/db/mongoose")
-
 app.use(registerRouter)
 
 
+// app.use("/static",express.static(path.join(__dirname, "/public")))
+app.use(cors()); 
+app.use(express.static(path.join(__dirname+'/frontend/build')));
+app.use(bodyParser.json());
 
-app.use(express.static("frontend/build"));
-
-
+app.get("/", function (req, res) {
+  res.render(path.join(__dirname+'/frontend/build'+'index.html'));
+})
 
 app.listen(port,()=>{
     console.log("you are on port ",port)
