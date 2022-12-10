c79ec54842cdcf1bd12531aac9a95531a25e7606
harshkumar <harsh1559.be20@gmail.com>
2022-12-10 10:04:54 +0530

    path correction in express

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ app.use(registerRouter)
 app.use(express.static(path.join(__dirname+'/frontend/build')));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, 'frontend/build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'))
  
 })
 
