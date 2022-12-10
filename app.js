dd44bb69ac91bcdf663473c11e9f8b1983b3b50a
harshkumar <harsh1559.be20@gmail.com>
2022-12-10 21:08:51 +0530

    push

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ require("./src/db/mongoose")
 app.use(registerRouter)
 
 
-  app.get("/", function (req, res) {
-    app.use(express.static(path.resolve(__dirname,'frontend','build')));
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-   
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
   })
+}
   
 
 
