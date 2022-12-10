6c2a1580060efcc6b87f38f34671bbddbb461f41
harshkumar <harsh1559.be20@gmail.com>
2022-12-10 21:12:59 +0530

    push

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(registerRouter)
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
   
