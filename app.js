2e94108e1d1bcb5b100828d064309d1f6550e941
harshkumar <harsh1559.be20@gmail.com>
2022-12-10 16:10:25 +0530

    push

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.use(registerRouter)
 
 app.use(express.static(path.join(__dirname+'/frontend/build')));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname+'/frontend/build/index.html'))
+app.get("/", function (req, res) {
+  res.send("hiii")
+  // res.sendFile(path.join(__dirname+'/frontend/build/index.html'))
  
 })
 
